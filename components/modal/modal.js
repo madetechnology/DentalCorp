852050f266cf1837845b7c59ea96d10f5c068baf
Angel Uriostegui <anguriostegui@outlook.com>
2024-06-08 11:46:14 -0400

    fixed

---
+++ components/modal/modal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import cn from "classnames";
+import styles from "./modal.module.css";
+import { createPortal } from "react-dom";
+import OutsideClickHandler from "react-outside-click-handler";
+
+export default function Modal({ children, visible, onClose, className }) {
+  React.useEffect(() => {
+    if (visible) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [visible]);
+
+  if (!visible) return null;
+
+  return createPortal(
+    <>
+      <div className={cn(styles.overlay, className)} />
+      <OutsideClickHandler onOutsideClick={onClose}>
+        <div>{children}</div>
+      </OutsideClickHandler>
+    </>,
+    document.body
+  );
+}
--- screens/about/video/video.js
+++ screens/about/video/video.js
@@ -1,14 +1,31 @@
+"use client";
+
 import React from "react";
 import cn from "classnames";
 import styles from "./video.module.css";
 import icons from "@/constants/icons";
+import Modal from "@/components/modal";
 
 export default function Video() {
+  const [visible, setVisible] = React.useState(false);
+
   return (
     <section className={styles.section}>
-      <div className={styles.play_button}>
+      <div className={styles.play_button} onClick={() => setVisible(true)}>
         <div className={styles.circle}>{icons.PlayRight}</div>
       </div>
+
+      <Modal visible={visible} onClose={() => setVisible(false)}>
+        <div className={styles.video}>
+          <iframe
+            title="video"
+            src="https://www.youtube.com/embed/4A2b6wJf6jw"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </Modal>
     </section>
   );
 }
