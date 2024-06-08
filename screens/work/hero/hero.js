b619f358fb2d1d882188fc558437ac460098da61
Angel Uriostegui <anguriostegui@outlook.com>
2024-06-08 10:23:41 -0400

    fox

--- screens/work/hero/hero.js
+++ screens/work/hero/hero.js
@@ -1,12 +1,23 @@
 import React from "react";
 import cn from "classnames";
 import styles from "./hero.module.css";
+import TextMarquee from "@/components/text-marquee";
+import AnimatedButton from "@/components/animated-button";
 
 export default function Hero() {
   return (
-    <section>
-      <div>
-        <div>Hero</div>
+    <section className={cn("section")}>
+      <TextMarquee>Our work.</TextMarquee>
+      <div className={cn("container")}>
+        <div className={styles.content}>
+          <AnimatedButton>
+            <p className={cn("label-large")}>See our work</p>
+          </AnimatedButton>
+          <p className={cn("paragraph-2x-large", styles.description)}>
+            We design and build digital products that solve real problems. Our
+            work is a blend of strategy, design, and technology.
+          </p>
+        </div>
       </div>
     </section>
   );
