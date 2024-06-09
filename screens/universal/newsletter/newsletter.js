ea2801aba720d6b0b85898f30953e077a0137cd0
Angel Uriostegui <anguriostegui@outlook.com>
2024-06-09 10:49:35 -0400

    fixed

--- screens/universal/newsletter/newsletter.js
+++ screens/universal/newsletter/newsletter.js
@@ -1,11 +1,16 @@
+"use client";
+
 import React from "react";
 import cn from "classnames";
 import styles from "./newsletter.module.css";
 import ScrollAnimatedText from "@/components/scroll-animated-text";
 import icons from "@/constants/icons";
 import Magnetic from "@/components/magnetic";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function Newsletter() {
+  const [state, handleSubmit] = useForm("mgeggwjn");
+
   return (
     <section className={cn("section")}>
       <div className={cn("container")}>
@@ -15,17 +20,40 @@ export default function Newsletter() {
             news—subscribe now and stay ahead of the curve!
           </ScrollAnimatedText>
 
-          <div className={styles.input}>
+          <form className={styles.form} onSubmit={handleSubmit}>
             <input
               className={cn("label-large")}
               type="email"
               placeholder="Enter your email address"
+              id="email"
+              name="email"
+              required
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
             />
 
             <Magnetic>
-              <button className={cn(styles.button)}>{icons.ArrowRight}</button>
+              <button
+                className={cn(styles.button)}
+                type="submit"
+                disabled={state.submitting}
+              >
+                {icons.ArrowRight}
+              </button>
             </Magnetic>
-          </div>
+          </form>
+
+          {state.succeeded && (
+            <div className={cn("paragraph-small", styles.success)}>
+              <p>
+                Thank you for subscribing! You'll receive a confirmation email
+                shortly.
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </section>
